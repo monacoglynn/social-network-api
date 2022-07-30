4b48b9e007ca9c35947f11bb4f78f4db863a4a95
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-07-30 14:57:12 -0700

    finished coding the User model. I have not tested it yet.

--- models/User.js
+++ models/User.js
@@ -5,10 +5,41 @@ const {
 
 const userSchema = new Schema({
     //should be unique, required and trimmed.
-    username: String,
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
     //required, and unique, must be valid email address, so find validation on mongoose
-    email: String,
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
     //thoughts will be an array of _id referencing the thought model
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
     //friends will be an array of _id that reference User model
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
     //create a virtual called friendCount that will retrieve the length of the friends array.
+}, {
+    toJSON: {
+        virtuals: true
+    },
+    id: false
 })
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length
+    });
+
+const User = model('user', userSchema);
+
+module.exports = User;
